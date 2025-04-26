1b86a06676c9cb638a2a3359d0bf18039f696b0b
orientye <orientye@qq.com>
2025-04-26 21:38:23 +0800

    auto-backward

--- experimental/test.py
+++ experimental/test.py
@@ -5,6 +5,17 @@ class Variable:
     def __init__(self, data):
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        f = self.creator
+        if f is not None:
+            x = f.input
+            x.grad = f.backward(self.grad)
+            x.backward()
 
 
 class Function:
@@ -12,6 +23,7 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
         self.input = input
         return output
 
@@ -81,7 +93,5 @@ a = A(x)
 b = B(a)
 y = C(b)
 y.grad = np.array(1.0)
-b.grad = C.backward(y.grad)
-a.grad = B.backward(b.grad)
-x.grad = A.backward(a.grad)
+y.backward()
 print(x.grad)
