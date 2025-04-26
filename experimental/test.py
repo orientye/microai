b1bcd1ec1133bac9a852aec65352ad31fc04d04c
orientye <orientye@qq.com>
2025-04-26 19:49:42 +0800

    microai

--- experimental/test.py
+++ experimental/test.py
@@ -1,1 +1,54 @@
 import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        self.input = input
+        self.output = output
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+
+class Cube(Function):
+    def forward(self, x):
+        return x ** 3
+
+
+class FourthPower(Function):
+    def forward(self, x):
+        return x ** 4
+
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
+def f(x):
+    A = Square()
+    B = Cube()
+    C = FourthPower()
+    return C(B(A(x)))
+
+
+x = Variable(np.array(1))
+dy = numerical_diff(f, x)
+print(dy)
