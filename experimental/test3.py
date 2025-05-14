a0f26ca06e3a9acac157f2e7593161106a45f97b
orientye <orientye@qq.com>
2025-05-14 08:18:17 +0800

    microai

--- experimental/test3.py
+++ experimental/test3.py
@@ -131,21 +131,6 @@ class Function:
         raise NotImplementedError()
 
 
-class Square(Function):
-    def forward(self, x):
-        y = x ** 2
-        return y
-
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = 2 * x * gy
-        return gx
-
-
-def square(x):
-    return Square()(x)
-
-
 class Add(Function):
     def forward(self, x0, x1):
         y = x0 + x1
@@ -159,25 +144,31 @@ def add(x0, x1):
     return Add()(x0, x1)
 
 
-x0 = Variable(np.array(1.0))
-x1 = Variable(np.array(1.0))
-t = add(x0, x1)
-y = add(x0, t)
+class Mul(Function):
+    def forward(self, x0, x1):
+        y = x0 * x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        return gy * x1, gy * x0
+
+
+def mul(x0, x1):
+    return Mul()(x0, x1)
+
+
+Variable.__add__ = add
+Variable.__mul__ = mul
+
+a = Variable(np.array(3.0))
+b = Variable(np.array(2.0))
+c = Variable(np.array(1.0))
+
+# y = add(mul(a, b), c)
+y = a * b + c
 y.backward()
-print(y.grad, t.grad)  # None None
-print(x0.grad, x1.grad)  # 2.0 1.0
 
-with using_config('enable_backprop', False):
-    x = Variable(np.array(2.0))
-    y = square(x)
-
-with no_grad():
-    x = Variable(np.array(2.0))
-    y = square(x)
-
-x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
-x.name = 'x'
-
-print(x.name)
-print(x.shape)
-print(x)
+print(y)
+print(a.grad)
+print(b.grad)
