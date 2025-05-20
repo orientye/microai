3173b85f1bc171573d6d8868f30a8d6568d69ebb
orientye <orientye@qq.com>
2025-05-21 07:33:34 +0800

    microai

---
+++ microai/__init__.py
@@ -0,0 +1,9 @@
+from microai.core import Variable
+from microai.core import Function
+from microai.core import using_config
+from microai.core import no_grad
+from microai.core import as_array
+from microai.core import as_variable
+from microai.core import init_core
+
+init_core()
