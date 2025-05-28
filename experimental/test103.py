699d178569d2f15167b5b1031c7a09a676d8e2d4
orientye <orientye@qq.com>
2025-05-28 08:17:11 +0800

    microai

--- experimental/test103.py
+++ experimental/test103.py
@@ -13,21 +13,20 @@ def rosenbrock(x0, x1):
 x0 = Variable(np.array(0.0))
 x1 = Variable(np.array(2.0))
 
-y = rosenbrock(x0, x1)
-y.backward()
-print(x0.grad, x1.grad)
+#y = rosenbrock(x0, x1)
+#y.backward()
+#print(x0.grad, x1.grad)
 
-# lr = 0.001
-# iters = 1000
-#
-# for i in range(iters):
-#     print(x0, x1)
-#
-#     y = rosenbrock(x0, x1)
-#
-#     x0.cleargrad()
-#     x1.cleargrad()
-#     y.backward()
-#
-#     x0.data -= lr * x0.grad
-#     x1.data -= lr * x1.grad
+lr = 0.001
+iters = 10000
+
+for i in range(iters):
+    print(x0, x1)
+    y = rosenbrock(x0, x1)
+
+    x0.cleargrad()
+    x1.cleargrad()
+    y.backward()
+
+    x0.data -= lr * x0.grad
+    x1.data -= lr * x1.grad
