bab8b544ae53f6596deab913d1d045d5d8a0ec30
orientye <orientye@qq.com>
2025-06-03 21:37:28 +0800

    microai

--- experimental/test103.py
+++ experimental/test103.py
@@ -1,5 +1,6 @@
 if '__file__' in globals():
     import os, sys
+
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
 from microai import Variable
@@ -13,9 +14,9 @@ def rosenbrock(x0, x1):
 x0 = Variable(np.array(0.0))
 x1 = Variable(np.array(2.0))
 
-#y = rosenbrock(x0, x1)
-#y.backward()
-#print(x0.grad, x1.grad)
+# y = rosenbrock(x0, x1)
+# y.backward()
+# print(x0.grad, x1.grad)
 
 lr = 0.001
 iters = 50000
