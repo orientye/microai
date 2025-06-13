d5c5cfd3b08ac94e7edb4a0878c26236504ba056
orientye <orientye@qq.com>
2025-06-13 08:04:40 +0800

    microai

--- microai/core.py
+++ microai/core.py
@@ -75,7 +75,7 @@ class Variable:
 
     def backward(self, retain_grad=False):
         if self.grad is None:
-            self.grad = np.ones_like(self.data)
+            self.grad = Variable(np.ones_like(self.data))
 
         funcs = []
         seen_set = set()
