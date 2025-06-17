7b0dd7b83671700fe2149ec2be7427c35cb30ec3
orientye <orientye@qq.com>
2025-06-17 08:23:02 +0800

    microai

---
+++ microai/funcs.py
@@ -0,0 +1,33 @@
+from microai import Function, cuda
+
+
+class Sin(Function):
+    def forward(self, x):
+        xp = cuda.get_array_module(x)
+        y = xp.sin(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * cos(x)
+        return gx
+
+
+def sin(x):
+    return Sin()(x)
+
+
+class Cos(Function):
+    def forward(self, x):
+        xp = cuda.get_array_module(x)
+        y = xp.cos(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * -sin(x)
+        return gx
+
+
+def cos(x):
+    return Cos()(x)
