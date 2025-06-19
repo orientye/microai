4b190fcf2b9c7f9c39160ef43cf7020a45199b5d
orientye <orientye@qq.com>
2025-06-19 09:24:34 +0800

    microai

---
+++ experimental/test107.py
@@ -0,0 +1,21 @@
+import numpy as np
+from microai import Variable
+import microai.funcs as F
+from microai.util import plot_dot_graph
+
+x = Variable(np.array(1.0))
+y = F.tanh(x)
+x.name = 'x'
+y.name = 'y'
+y.backward(create_graph=True)
+
+iters = 1
+
+for i in range(iters):
+    gx = x.grad
+    x.cleargrad()
+    gx.backward(create_graph=True)
+
+gx = x.grad
+gx.name = 'gx' + str(iters + 1)
+plot_dot_graph(gx, verbose=False, to_file='tanh.png')
