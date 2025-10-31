947a98598a149a76f1d7894181d5d406d1983a6f
orientye <orientye@qq.com>
2025-10-31 08:52:51 +0800

    microai

---
+++ microai/layers.py
@@ -0,0 +1,11 @@
+from microai import Parameter
+
+
+class Layer:
+    def __init__(self):
+        self._params = set()
+
+    def __setattr__(self, name, value):
+        if isinstance(value, (Parameter, Layer)):
+            self._params.add(name)
+        super().__setattr__(name, value)
