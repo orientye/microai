3da5ebdf70811a37ca024f17349930da1ac99f08
orientye <orientye@qq.com>
2025-11-03 09:04:01 +0800

    microai

--- microai/layers.py
+++ microai/layers.py
@@ -1,3 +1,5 @@
+import weakref
+
 from microai import Parameter
 
 
@@ -9,3 +11,27 @@ class Layer:
         if isinstance(value, (Parameter, Layer)):
             self._params.add(name)
         super().__setattr__(name, value)
+
+    def __call__(self, *inputs):
+        outputs = self.forward(*inputs)
+        if not isinstance(outputs, tuple):
+            outputs = (outputs,)
+        self.inputs = [weakref.ref(x) for x in inputs]
+        self.outputs = [weakref.ref(y) for y in outputs]
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, inputs):
+        raise NotImplementedError()
+
+    def params(self):
+        for name in self._params:
+            obj = self.__dict__[name]
+
+            if isinstance(obj, Layer):
+                yield from obj.params()
+            else:
+                yield obj
+
+    def cleargrads(self):
+        for param in self.params():
+            param.cleargrad()
