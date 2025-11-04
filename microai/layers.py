306af6c17b9978424e7c1d76288016968d961c37
orientye <orientye@qq.com>
2025-11-04 08:51:58 +0800

    microai

--- microai/layers.py
+++ microai/layers.py
@@ -1,5 +1,7 @@
 import weakref
-
+import numpy as np
+import microai.funcs as F
+from microai import cuda
 from microai import Parameter
 
 
@@ -35,3 +37,34 @@ class Layer:
     def cleargrads(self):
         for param in self.params():
             param.cleargrad()
+
+
+class Linear(Layer):
+    def __init__(self, out_size, nobias=False, dtype=np.float32, in_size=None):
+        super().__init__()
+        self.in_size = in_size
+        self.out_size = out_size
+        self.dtype = dtype
+
+        self.W = Parameter(None, name='W')
+        if self.in_size is not None:
+            self._init_W()
+
+        if nobias:
+            self.b = None
+        else:
+            self.b = Parameter(np.zeros(out_size, dtype=dtype), name='b')
+
+    def _init_W(self, xp=np):
+        I, O = self.in_size, self.out_size
+        W_data = xp.random.randn(I, O).astype(self.dtype) * np.sqrt(1 / I)
+        self.W.data = W_data
+
+    def forward(self, x):
+        if self.W.data is None:
+            self.in_size = x.shape[1]
+            xp = cuda.get_array_module(x)
+            self._init_W(xp)
+
+        y = F.linear(x, self.W, self.b)
+        return y
