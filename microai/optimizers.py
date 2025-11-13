626eb0e37be1887d2dbca460b48debb812c4dc12
orientye <orientye@qq.com>
2025-11-13 08:56:28 +0800

    microai

--- microai/optimizers.py
+++ microai/optimizers.py
@@ -21,3 +21,12 @@ class Optimizer:
 
     def add_hook(self, f):
         self.hooks.append(f)
+
+# Stochastic Gradient Descent
+class SGD(Optimizer):
+    def __init__(self, lr=0.01):
+        super().__init__()
+        self.lr = lr
+
+    def update_one(self, param):
+        param.data -= self.lr * param.grad.data
