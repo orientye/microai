05292ac32203a44aef2e41af81fd55db0d92fba6
orientye <orientye@qq.com>
2025-12-29 08:49:48 +0800

    microai

--- microai/datasets.py
+++ microai/datasets.py
@@ -1,4 +1,8 @@
+import gzip
 import numpy as np
+import matplotlib.pyplot as plt
+from microai.util import get_file
+
 
 class Dataset:
     def __init__(self, train=True, transform=None, target_transform=None):
@@ -27,9 +31,11 @@ class Dataset:
     def prepare(self):
         pass
 
+
 class Spiral(Dataset):
     def prepare(self):
         self.data, self.label = get_spiral(self.train)
+
 
 def get_spiral(train=True):
     seed = 1984 if train else 2020
@@ -54,3 +60,51 @@ def get_spiral(train=True):
     x = x[indices]
     t = t[indices]
     return x, t
+
+
+class MNIST(Dataset):
+
+    def __init__(self, train=True,
+                 transform=None,
+                 target_transform=None):
+        super().__init__(train, transform, target_transform)
+
+    def prepare(self):
+        url = 'http://yann.lecun.com/exdb/mnist/'
+        train_files = {'target': 'train-images-idx3-ubyte.gz',
+                       'label': 'train-labels-idx1-ubyte.gz'}
+        test_files = {'target': 't10k-images-idx3-ubyte.gz',
+                      'label': 't10k-labels-idx1-ubyte.gz'}
+
+        files = train_files if self.train else test_files
+        data_path = get_file(url + files['target'])
+        label_path = get_file(url + files['label'])
+
+        self.data = self._load_data(data_path)
+        self.label = self._load_label(label_path)
+
+    def _load_label(self, filepath):
+        with gzip.open(filepath, 'rb') as f:
+            labels = np.frombuffer(f.read(), np.uint8, offset=8)
+        return labels
+
+    def _load_data(self, filepath):
+        with gzip.open(filepath, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=16)
+        data = data.reshape(-1, 1, 28, 28)
+        return data
+
+    def show(self, row=10, col=10):
+        H, W = 28, 28
+        img = np.zeros((H * row, W * col))
+        for r in range(row):
+            for c in range(col):
+                img[r * H:(r + 1) * H, c * W:(c + 1) * W] = self.data[
+                    np.random.randint(0, len(self.data) - 1)].reshape(H, W)
+        plt.imshow(img, cmap='gray', interpolation='nearest')
+        plt.axis('off')
+        plt.show()
+
+    @staticmethod
+    def labels():
+        return {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
