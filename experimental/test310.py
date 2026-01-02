28c143ed68daf5e00b4e04d92babeb43c56f42e6
orientye <orientye@qq.com>
2026-01-02 16:50:48 +0800

    microai

---
+++ experimental/test310.py
@@ -0,0 +1,49 @@
+import microai
+import microai.funcs as F
+from microai.models import MLP
+from microai import optimizers
+from microai import DataLoader
+
+max_epoch = 5
+batch_size = 100
+hidden_size = 1000
+
+train_set = microai.datasets.MNIST(train=True)
+test_set = microai.datasets.MNIST(train=False)
+train_loader = DataLoader(train_set, batch_size)
+test_loader = DataLoader(test_set, batch_size, shuffle=False)
+
+model = MLP((hidden_size, 10))
+optimizer = optimizers.SGD().setup(model)
+#model = MLP((hidden_size, hidden_size, 10), activation=F.relu)
+#optimizer = optimizers.Adam().setup(model)
+
+for epoch in range(max_epoch):
+    sum_loss, sum_acc = 0, 0
+
+    for x, t in train_loader:
+        y = model(x)
+        loss = F.softmax_cross_entropy(y, t)
+        acc = F.accuracy(y, t)
+        model.cleargrads()
+        loss.backward()
+        optimizer.update()
+
+        sum_loss += float(loss.data) * len(t)
+        sum_acc += float(acc.data) * len(t)
+
+    print('epoch: {}'.format(epoch+1))
+    print('train loss: {:.4f}, accuracy: {:.4f}'.format(
+        sum_loss / len(train_set), sum_acc / len(train_set)))
+
+    sum_loss, sum_acc = 0, 0
+    with microai.no_grad():
+        for x, t in test_loader:
+            y = model(x)
+            loss = F.softmax_cross_entropy(y, t)
+            acc = F.accuracy(y, t)
+            sum_loss += float(loss.data) * len(t)
+            sum_acc += float(acc.data) * len(t)
+
+    print('test loss: {:.4f}, accuracy: {:.4f}'.format(
+        sum_loss / len(test_set), sum_acc / len(test_set)))
