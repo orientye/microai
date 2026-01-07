50d6540ed78622e2899f6ab0f0f6f537e5c4c01f
orientye <orientye@qq.com>
2026-01-07 09:26:20 +0800

    microai

---
+++ experimental/test401.py
@@ -0,0 +1,42 @@
+import time
+
+import microai
+import microai.funcs as F
+from microai.models import MLP
+from microai import optimizers
+from microai import DataLoader
+
+max_epoch = 5
+batch_size = 100
+hidden_size = 1000
+
+train_set = microai.datasets.MNIST(train=True)
+test_set = microai.datasets.MNIST(train=False)
+train_loader = DataLoader(train_set, batch_size)
+test_loader = DataLoader(test_set, batch_size, shuffle=False)
+
+model = MLP((hidden_size, 10))
+optimizer = optimizers.SGD().setup(model)
+# model = MLP((hidden_size, hidden_size, 10), activation=F.relu)
+# optimizer = optimizers.Adam().setup(model)
+
+# GPU mode
+if microai.cuda.gpu_enable:
+    train_loader.to_gpu()
+    model.to_gpu()
+
+for epoch in range(max_epoch):
+    start = time.time()
+    sum_loss = 0
+
+    for x, t in train_loader:
+        y = model(x)
+        loss = F.softmax_cross_entropy(y, t)
+        model.cleargrads()
+        loss.backward()
+        optimizer.update()
+        sum_loss += float(loss.data) * len(t)
+
+    elapsed_time = time.time() - start
+    print('epoch: {}, loss: {:.4f}, time: {:.4f}[sec]'.format(
+        epoch + 1, sum_loss / len(train_set), elapsed_time))
