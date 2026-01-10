e09301ca155b6183d1e0a6aed2134b3fbfadf09c
orientye <orientye@qq.com>
2026-01-10 20:31:34 +0800

    microai

--- microai/__init__.py
+++ microai/__init__.py
@@ -3,9 +3,11 @@ from microai.core import Parameter
 from microai.core import Function
 from microai.core import using_config
 from microai.core import no_grad
+from microai.core import test_mode
 from microai.core import as_array
 from microai.core import as_variable
 from microai.core import init_core
+from microai.core import Config
 from microai.layers import Layer
 from microai.models import Model
 from microai.datasets import Dataset
@@ -14,5 +16,8 @@ from microai.dataloaders import DataLoader
 import microai.cuda
 import microai.funcs
 import microai.datasets
+import microai.dataloaders
+import microai.optimizers
+import microai.layers
 
 init_core()
