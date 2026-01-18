e652809d2fe51ef51d775f1e98f5f8fff3e714fc
orientye <orientye@qq.com>
2026-01-18 10:53:53 +0800

    microai

---
+++ microai/conv.py
@@ -0,0 +1,41 @@
+from microai.core import Function, as_variable
+
+
+class Im2col(Function):
+    def __init__(self, kernel_size, stride, pad, to_matrix):
+        super().__init__()
+        self.input_shape = None
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = pad
+        self.to_matrix = to_matrix
+
+
+def im2col(x, kernel_size, stride=1, pad=0, to_matrix=True):
+    """Extract patches from an image based on the filter.
+
+    Args:
+        x (`dezero.Variable` or `ndarray`): Input variable of shape
+            `(N, C, H, W)`
+        kernel_size (int or (int, int)): Size of kernel.
+        stride (int or (int, int)): Stride of kernel.
+        pad (int or (int, int)): Spatial padding width for input arrays.
+        to_matrix (bool): If True the `col` will be reshaped to 2d array whose
+            shape is `(N*OH*OW, C*KH*KW)`
+
+    Returns:
+        `dezero.Variable`: Output variable. If the `to_matrix` is False, the
+            output shape is `(N, C, KH, KW, OH, OW)`, otherwise
+            `(N*OH*OW, C*KH*KW)`.
+
+    Notation:
+    - `N` is the batch size.
+    - `C` is the number of the input channels.
+    - `H` and `W` are the height and width of the input image, respectively.
+    - `KH` and `KW` are the height and width of the filters, respectively.
+    - `SH` and `SW` are the strides of the filter.
+    - `PH` and `PW` are the spatial padding sizes.
+    - `OH` and `OW` are the the height and width of the output, respectively.
+    """
+    y = Im2col(kernel_size, stride, pad, to_matrix)(x)
+    return y
