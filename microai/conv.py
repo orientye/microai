38859942c10748df07723f0eee9a1e8a38379c5f
orientye <orientye@qq.com>
2026-01-18 14:51:43 +0800

    microai

--- microai/conv.py
+++ microai/conv.py
@@ -1,5 +1,41 @@
 from microai.core import Function, as_variable
+from microai.util import pair, get_conv_outsize
+from microai import cuda
 
+#将图像数据转换为适合卷积运算的矩阵形式 GPU方式
+def _im2col_gpu(img, kernel_size, stride, pad):
+    n, c, h, w = img.shape
+    kh, kw = pair(kernel_size)
+    sy, sx = pair(stride)
+    ph, pw = pair(pad)
+    out_h = get_conv_outsize(h, kh, sy, ph)
+    out_w = get_conv_outsize(w, kw, sx, pw)
+    dy, dx = 1, 1
+    col = cuda.cupy.empty((n, c, kh, kw, out_h, out_w), dtype=img.dtype)
+
+    cuda.cupy.ElementwiseKernel(
+        'raw T img, int32 h, int32 w, int32 out_h, int32 out_w,'
+        'int32 kh, int32 kw, int32 sy, int32 sx, int32 ph, int32 pw,'
+        'int32 dy, int32 dx',
+        'T col',
+        '''
+           int c0 = i / (kh * kw * out_h * out_w);
+           int ky = i / (kw * out_h * out_w) % kh;
+           int kx = i / (out_h * out_w) % kw;
+           int out_y = i / out_w % out_h;
+           int out_x = i % out_w;
+           int in_y = ky * dy + out_y * sy - ph;
+           int in_x = kx * dx + out_x * sx - pw;
+           if (in_y >= 0 && in_y < h && in_x >= 0 && in_x < w) {
+             col = img[in_x + w * (in_y + h * c0)];
+           } else {
+             col = 0;
+           }
+        ''',
+        'im2col')(img.reduced_view(),
+                  h, w, out_h, out_w, kh, kw, sy, sx, ph, pw, dy, dx, col)
+
+    return col
 
 class Im2col(Function):
     def __init__(self, kernel_size, stride, pad, to_matrix):
