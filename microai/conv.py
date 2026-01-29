6854e05ccdf754c638102f8da3e0a8df056fd89e
orientye <orientye@qq.com>
2026-01-29 09:17:50 +0800

    microai

--- microai/conv.py
+++ microai/conv.py
@@ -4,6 +4,7 @@ from microai.core import Function, as_variable
 from microai.util import pair, get_conv_outsize
 from microai import cuda
 
+
 def im2col_array(img, kernel_size, stride, pad, to_matrix=True):
     N, C, H, W = img.shape
     KH, KW = pair(kernel_size)
@@ -58,7 +59,8 @@ def col2im_array(col, img_shape, kernel_size, stride, pad, to_matrix=True):
                 img[:, :, j:j_lim:SH, i:i_lim:SW] += col[:, :, j, i, :, :]
         return img[:, :, PH:H + PH, PW:W + PW]
 
-#将图像数据转换为适合卷积运算的矩阵形式 GPU方式
+
+# 将图像数据转换为适合卷积运算的矩阵形式 GPU方式
 def _im2col_gpu(img, kernel_size, stride, pad):
     n, c, h, w = img.shape
     kh, kw = pair(kernel_size)
@@ -92,6 +94,7 @@ def _im2col_gpu(img, kernel_size, stride, pad):
                   h, w, out_h, out_w, kh, kw, sy, sx, ph, pw, dy, dx, col)
 
     return col
+
 
 class Im2col(Function):
     def __init__(self, kernel_size, stride, pad, to_matrix):
@@ -143,6 +146,7 @@ def im2col(x, kernel_size, stride=1, pad=0, to_matrix=True):
     y = Im2col(kernel_size, stride, pad, to_matrix)(x)
     return y
 
+
 def _col2im_gpu(col, sy, sx, ph, pw, h, w):
     n, c, kh, kw, out_h, out_w = col.shape
     dx, dy = 1, 1
@@ -177,3 +181,118 @@ def _col2im_gpu(col, sy, sx, ph, pw, h, w):
         'col2im')(col.reduced_view(),
                   h, w, out_h, out_w, kh, kw, sy, sx, ph, pw, dx, dy, img)
     return img
+
+
+class Conv2d(Function):
+    def __init__(self, stride=1, pad=0):
+        super().__init__()
+        self.stride = pair(stride)
+        self.pad = pair(pad)
+
+    def forward(self, x, W, b):
+        xp = cuda.get_array_module(x)
+
+        KH, KW = W.shape[2:]
+        col = im2col_array(x, (KH, KW), self.stride, self.pad, to_matrix=False)
+
+        y = xp.tensordot(col, W, ((1, 2, 3), (1, 2, 3)))
+        if b is not None:
+            y += b
+        y = xp.rollaxis(y, 3, 1)
+        # y = np.transpose(y, (0, 3, 1, 2))
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        # ==== gx ====
+        gx = deconv2d(gy, W, b=None, stride=self.stride, pad=self.pad,
+                      outsize=(x.shape[2], x.shape[3]))
+        # ==== gW ====
+        gW = Conv2DGradW(self)(x, gy)
+        # ==== gb ====
+        gb = None
+        if b.data is not None:
+            gb = gy.sum(axis=(0, 2, 3))
+        return gx, gW, gb
+
+
+def conv2d(x, W, b=None, stride=1, pad=0):
+    return Conv2d(stride, pad)(x, W, b)
+
+
+class Deconv2d(Function):
+    def __init__(self, stride=1, pad=0, outsize=None):
+        super().__init__()
+        self.stride = pair(stride)
+        self.pad = pair(pad)
+        self.outsize = outsize
+
+    def forward(self, x, W, b):
+        xp = cuda.get_array_module(x)
+
+        Weight = W
+        SH, SW = self.stride
+        PH, PW = self.pad
+        C, OC, KH, KW = Weight.shape
+        N, C, H, W = x.shape
+        if self.outsize is None:
+            out_h = get_deconv_outsize(H, KH, SH, PH)
+            out_w = get_deconv_outsize(W, KW, SW, PW)
+        else:
+            out_h, out_w = pair(self.outsize)
+        img_shape = (N, OC, out_h, out_w)
+
+        gcol = xp.tensordot(Weight, x, (0, 1))
+        gcol = xp.rollaxis(gcol, 3)
+        y = col2im_array(gcol, img_shape, (KH, KW), self.stride, self.pad,
+                         to_matrix=False)
+        # b, k, h, w
+        if b is not None:
+            self.no_bias = True
+            y += b.reshape((1, b.size, 1, 1))
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+
+        # ==== gx ====
+        gx = conv2d(gy, W, b=None, stride=self.stride, pad=self.pad)
+        # ==== gW ====
+        f = Conv2DGradW(self)
+        gW = f(gy, x)
+        # ==== gb ====
+        gb = None
+        if b.data is not None:
+            gb = gy.sum(axis=(0, 2, 3))
+        return gx, gW, gb
+
+
+def deconv2d(x, W, b=None, stride=1, pad=0, outsize=None):
+    return Deconv2d(stride, pad, outsize)(x, W, b)
+
+
+class Conv2DGradW(Function):
+    def __init__(self, conv2d):
+        W = conv2d.inputs[1]
+        kh, kw = W.shape[2:]
+        self.kernel_size = (kh, kw)
+        self.stride = conv2d.stride
+        self.pad = conv2d.pad
+
+    def forward(self, x, gy):
+        xp = cuda.get_array_module(x)
+
+        col = im2col_array(x, self.kernel_size, self.stride, self.pad,
+                           to_matrix=False)
+        gW = xp.tensordot(gy, col, ((0, 2, 3), (0, 4, 5)))
+        return gW
+
+    def backward(self, gys):
+        x, gy = self.inputs
+        gW, = self.outputs
+
+        xh, xw = x.shape[2:]
+        gx = deconv2d(gy, gW, stride=self.stride, pad=self.pad,
+                      outsize=(xh, xw))
+        ggy = conv2d(x, gW, stride=self.stride, pad=self.pad)
+        return gx, ggy
