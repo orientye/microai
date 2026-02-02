50747ed59a900fbb35d81bf6188ed4ce929d7ee8
orientye <orientye@qq.com>
2026-02-03 07:57:25 +0800

    microai

--- microai/conv.py
+++ microai/conv.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-from microai.core import Function, as_variable
+from microai.core import Function
 from microai.util import pair, get_conv_outsize, get_deconv_outsize
+from microai.funcs import  broadcast_to
 from microai import cuda
 
 
@@ -328,3 +329,115 @@ class Conv2DGradW(Function):
                       outsize=(xh, xw))
         ggy = conv2d(x, gW, stride=self.stride, pad=self.pad)
         return gx, ggy
+
+
+class Pooling(Function):
+    def __init__(self, kernel_size, stride=1, pad=0):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        col = im2col_array(x, self.kernel_size, self.stride, self.pad,
+                           to_matrix=False)
+
+        N, C, KH, KW, OH, OW = col.shape
+        col = col.reshape(N, C, KH * KW, OH, OW)
+        self.indexes = col.argmax(axis=2)
+        y = col.max(axis=2)
+        return y
+
+    def backward(self, gy):
+        return Pooling2DGrad(self)(gy)
+
+
+class Pooling2DGrad(Function):
+    def __init__(self, mpool2d):
+        self.mpool2d = mpool2d
+        self.kernel_size = mpool2d.kernel_size
+        self.stride = mpool2d.stride
+        self.pad = mpool2d.pad
+        self.input_shape = mpool2d.inputs[0].shape
+        self.dtype = mpool2d.inputs[0].dtype
+        self.indexes = mpool2d.indexes
+
+    def forward(self, gy):
+        xp = cuda.get_array_module(gy)
+
+        N, C, OH, OW = gy.shape
+        N, C, H, W = self.input_shape
+        KH, KW = pair(self.kernel_size)
+
+        gcol = xp.zeros((N * C * OH * OW * KH * KW), dtype=self.dtype)
+
+        indexes = (self.indexes.ravel()
+                   + xp.arange(0, self.indexes.size * KH * KW, KH * KW))
+
+        gcol[indexes] = gy.ravel()
+        gcol = gcol.reshape(N, C, OH, OW, KH, KW)
+        gcol = xp.swapaxes(gcol, 2, 4)
+        gcol = xp.swapaxes(gcol, 3, 5)
+
+        gx = col2im_array(gcol, (N, C, H, W), self.kernel_size, self.stride,
+                          self.pad, to_matrix=False)
+        return gx
+
+    def backward(self, ggx):
+        f = Pooling2DWithIndexes(self.mpool2d)
+        return f(ggx)
+
+
+class Pooling2DWithIndexes(Function):
+    def __init__(self, mpool2d):
+        self.kernel_size = mpool2d.kernel_size
+        self.stride = mpool2d.stride
+        self.pad = mpool2d.pad
+        self.input_shpae = mpool2d.inputs[0].shape
+        self.dtype = mpool2d.inputs[0].dtype
+        self.indexes = mpool2d.indexes
+
+    def forward(self, x):
+        col = im2col_array(x, self.kernel_size, self.stride, self.pad,
+                           to_matrix=False)
+        N, C, KH, KW, OH, OW = col.shape
+        col = col.reshape(N, C, KH * KW, OH, OW)
+        col = col.transpose(0, 1, 3, 4, 2).reshape(-1, KH * KW)
+        indexes = self.indexes.ravel()
+        col = col[np.arange(len(indexes)), indexes]
+        return col.reshape(N, C, OH, OW)
+
+
+def pooling(x, kernel_size, stride=1, pad=0):
+    return Pooling(kernel_size, stride, pad)(x)
+
+
+class AveragePooling(Function):
+    def __init__(self, kernel_size, stride=1, pad=0):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = pad
+        self.input_shape = None
+
+    def forward(self, x):
+        self.input_shape = x.shape
+        col = im2col_array(x, self.kernel_size, self.stride, self.pad,
+                           to_matrix=False)
+        y = col.mean(axis=(2, 3))
+        return y
+
+    def backward(self, gy):
+        # TODO(Koki): This is simple implementation
+        N, C, OH, OW = gy.shape
+        KW, KH = pair(self.kernel_size)
+        gy /= (KW * KH)
+        gcol = broadcast_to(gy.reshape(-1), (KH, KW, N * C * OH * OW))
+        gcol = gcol.reshape(KH, KW, N, C, OH, OW).transpose(2, 3, 0, 1, 4, 5)
+        gx = col2im(gcol, self.input_shape, self.kernel_size, self.stride,
+                    self.pad, to_matrix=False)
+        return gx
+
+
+def average_pooling(x, kernel_size, stride=1, pad=0):
+    return AveragePooling(kernel_size, stride, pad)(x)
