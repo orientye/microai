c638517aa97c19a66ed259c2f9db2f5e77db4d11
orientye <orientye@qq.com>
2026-02-23 20:16:37 +0800

    microai

--- microai/layers.py
+++ microai/layers.py
@@ -6,6 +6,7 @@ from microai import cuda
 from microai import Parameter
 from microai.util import pair
 
+
 class Layer:
     def __init__(self):
         self._params = set()
@@ -206,3 +207,31 @@ class Deconv2d(Layer):
 
         y = F.deconv2d(x, self.W, self.b, self.stride, self.pad)
         return y
+
+
+class RNN(Layer):
+    def __init__(self, hidden_size, in_size=None):
+        """An Elman RNN with tanh.
+
+        Args:
+            hidden_size (int): The number of features in the hidden state.
+            in_size (int): The number of features in the input. If unspecified
+            or `None`, parameter initialization will be deferred until the
+            first `__call__(x)` at which time the size will be determined.
+
+        """
+        super().__init__()
+        self.x2h = Linear(hidden_size, in_size=in_size)
+        self.h2h = Linear(hidden_size, in_size=in_size, nobias=True)
+        self.h = None
+
+    def reset_state(self):
+        self.h = None
+
+    def forward(self, x):
+        if self.h is None:
+            h_new = F.tanh(self.x2h(x))
+        else:
+            h_new = F.tanh(self.x2h(x) + self.h2h(self.h))
+        self.h = h_new
+        return h_new
