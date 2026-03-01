7753f61bf6120518ab6659201dbd3e41b57bf271
orientye <orientye@qq.com>
2026-03-02 07:44:13 +0800

    microai

--- experimental/test407.py
+++ experimental/test407.py
@@ -1,7 +1,32 @@
 import numpy as np
+import microai
+from microai import Model
 import microai.layers as L
 
 rnn = L.RNN(10)
-x = np.random.rand(1 , 1)
+x = np.random.rand(1, 1)
 h = rnn(x)
 print(h.shape)
+
+# Hyperparameters
+max_epoch = 100
+hidden_size = 100
+bptt_length = 30
+
+train_set = microai.datasets.SinCurve(train=True)
+seqlen = len(train_set)
+
+
+class SimpleRNN(Model):
+    def __init__(self, hidden_size, out_size):
+        super().__init__()
+        self.rnn = L.RNN(hidden_size)
+        self.fc = L.Linear(out_size)
+
+    def reset_state(self):
+        self.rnn.reset_state()
+
+    def __call__(self, x):
+        h = self.rnn(x)
+        y = self.fc(h)
+        return y
