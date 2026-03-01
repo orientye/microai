ed2a722537e210bdfb2937c17f5486df9d094b59
orientye <orientye@qq.com>
2026-03-02 07:49:22 +0800

    microai

--- experimental/test407.py
+++ experimental/test407.py
@@ -2,6 +2,8 @@ import numpy as np
 import microai
 from microai import Model
 import microai.layers as L
+import microai.funcs as F
+import matplotlib.pyplot as plt
 
 rnn = L.RNN(10)
 x = np.random.rand(1, 1)
@@ -30,3 +32,45 @@ class SimpleRNN(Model):
         h = self.rnn(x)
         y = self.fc(h)
         return y
+
+
+model = SimpleRNN(hidden_size, 1)
+optimizer = microai.optimizers.Adam().setup(model)
+
+# Start training.
+for epoch in range(max_epoch):
+    model.reset_state()
+    loss, count = 0, 0
+
+    for x, t in train_set:
+        x = x.reshape(1, 1)
+        y = model(x)
+        loss += F.mean_squared_error(y, t)
+        count += 1
+
+        if count % bptt_length == 0 or count == seqlen:
+            model.cleargrads()
+            loss.backward()
+            loss.unchain_backward()
+            optimizer.update()
+
+    avg_loss = float(loss.data) / count
+    print('| epoch %d | loss %f' % (epoch + 1, avg_loss))
+
+# Plot
+xs = np.cos(np.linspace(0, 4 * np.pi, 1000))
+model.reset_state()
+pred_list = []
+
+with microai.no_grad():
+    for x in xs:
+        x = np.array(x).reshape(1, 1)
+        y = model(x)
+        pred_list.append(float(y.data))
+
+plt.plot(np.arange(len(xs)), xs, label='y=cos(x)')
+plt.plot(np.arange(len(xs)), pred_list, label='predict')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
