f787edf905446813623508f005da7316d54027bc
orientye <orientye@qq.com>
2026-03-14 21:29:40 +0800

    microai

---
+++ experimental-didl/test1103.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import torch
+
+def nadaraya_watson(x_train, y_train, x_val, kernel):
+    dists = x_train.reshape((-1, 1)) - x_val.reshape((1, -1))
+    # Each column/row corresponds to each query/key
+    k = kernel(dists).type(torch.float32)
+    # Normalization over keys for each query
+    attention_w = k / k.sum(0)
+    y_hat = y_train@attention_w
+    return y_hat, attention_w
+
+def plot(x_train, y_train, x_val, y_val, kernels, names, attention=False):
+    fig, axes = plt.subplots(1, 4, sharey=True, figsize=(12, 3))
+    for kernel, name, ax in zip(kernels, names, axes):
+        y_hat, attention_w = nadaraya_watson(x_train, y_train, x_val, kernel)
+        if attention:
+            pcm = ax.imshow(attention_w.detach().numpy(), cmap='Reds')
+        else:
+            ax.plot(x_val, y_hat)
+            ax.plot(x_val, y_val, 'm--')
+            ax.plot(x_train, y_train, 'o', alpha=0.5);
+        ax.set_xlabel(name)
+        if not attention:
+            ax.legend(['y_hat', 'y'])
+    if attention:
+        fig.colorbar(pcm, ax=axes, shrink=0.7)
+
+def f(x):
+    return 2 * torch.sin(x) + x
+
+n = 40
+x_train, _ = torch.sort(torch.rand(n) * 5)
+y_train = f(x_train) + torch.randn(n)
+x_val = torch.arange(0, 5, 0.1)
+y_val = f(x_val)
+
+# Define some kernels
+def gaussian(x):
+    return torch.exp(-x**2 / 2)
+
+def boxcar(x):
+    return torch.abs(x) < 1.0
+
+def constant(x):
+    return 1.0 + 0 * x
+
+def epanechikov(x):
+    return torch.max(1 - torch.abs(x), torch.zeros_like(x))
+
+fig, axes = plt.subplots(1, 4, sharey=True, figsize=(12, 3))
+
+kernels = (gaussian, boxcar, constant, epanechikov)
+names = ('Gaussian', 'Boxcar', 'Constant', 'Epanechikov')
+
+#plot(x_train, y_train, x_val, y_val, kernels, names)
+plot(x_train, y_train, x_val, y_val, kernels, names, attention=True)
+
+plt.show()
