23c3fb5c95aab0f4a68d5bdbbf1b07f5d7219770
orientye <orientye@qq.com>
2026-03-30 09:29:54 +0800

    microai

---
+++ experimental-didl/test1102_.py
@@ -0,0 +1,33 @@
+# https://en.d2l.ai/chapter_attention-mechanisms-and-transformers/attention-pooling.html
+
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+from d2l import torch as d2l
+
+d2l.use_svg_display()
+
+# Define some kernels
+def gaussian(x):
+    return torch.exp(-x**2 / 2)
+
+def boxcar(x):
+    return torch.abs(x) < 1.0
+
+def constant(x):
+    return 1.0 + 0 * x
+
+def epanechikov(x):
+    return torch.max(1 - torch.abs(x), torch.zeros_like(x))
+
+fig, axes = d2l.plt.subplots(1, 4, sharey=True, figsize=(12, 3))
+
+kernels = (gaussian, boxcar, constant, epanechikov)
+names = ('Gaussian', 'Boxcar', 'Constant', 'Epanechikov')
+x = torch.arange(-2.5, 2.5, 0.1)
+for kernel, name, ax in zip(kernels, names, axes):
+    ax.plot(x.detach().numpy(), kernel(x).detach().numpy())
+    ax.set_xlabel(name)
+
+d2l.plt.show()
