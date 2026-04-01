5998b997d5be7b4cec4aa4f9baa7e9e023f3db9c
orientye <orientye@qq.com>
2026-04-01 09:30:17 +0800

    microai

--- experimental-didl/test1103.py
+++ experimental-didl/test1103.py
@@ -50,7 +50,7 @@ def constant(x):
 def epanechikov(x):
     return torch.max(1 - torch.abs(x), torch.zeros_like(x))
 
-fig, axes = plt.subplots(1, 4, sharey=True, figsize=(12, 4))
+#fig, axes = plt.subplots(1, 4, sharey=True, figsize=(12, 4))
 
 kernels = (gaussian, boxcar, constant, epanechikov)
 names = ('Gaussian', 'Boxcar', 'Constant', 'Epanechikov')
