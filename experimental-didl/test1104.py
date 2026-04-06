49bc3b6f36ebc0c2d625ff8b3d6a5133797ddc3b
orientye <orientye@qq.com>
2026-04-06 19:13:22 +0800

    micorai

--- experimental-didl/test1104.py
+++ experimental-didl/test1104.py
@@ -12,6 +12,14 @@ def masked_softmax(X, valid_lens):  #@save
         maxlen = X.size(1)
         mask = torch.arange((maxlen), dtype=torch.float32,
                             device=X.device)[None, :] < valid_len[:, None]
+        # X形状通常为(batch_size, num_queries, num_keys)
+        # 在masked_softmax的实现里，它执行了X.reshape(-1, shape[-1])。这意味着传入_sequence_mask的X实际上被展平成了二维：(batch_size * num_queries, num_keys)。
+        # 因此，maxlen = X.size(1) 获取的是num_keys（即最后一维的长度），而不是num_queries。
+        # 创建掩码矩阵
+        # torch.arange(maxlen): 生成[0, 1, 2, ..., maxlen - 1]
+        # [None,:]: 添加维度，形状变为(1, maxlen)
+        # valid_len[:, None]: 将valid_len变为(batch_size, 1)
+        # 广播比较，生成形状(batch_size, maxlen)的布尔掩码
         X[~mask] = value
         return X
 
