49bd46a4c250756cd321ea48532efeed7a9463c8
orientye <orientye@qq.com>
2026-04-24 21:04:24 +0800

    microai

---
+++ experimental-didl/test1112.py
@@ -0,0 +1,15 @@
+# https://en.d2l.ai/chapter_attention-mechanisms-and-transformers/self-attention-and-positional-encoding.html
+
+
+import math
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+num_hiddens, num_heads = 100, 5
+attention = d2l.MultiHeadAttention(num_hiddens, num_heads, 0.5)
+batch_size, num_queries, valid_lens = 2, 4, torch.tensor([3, 2])
+X = torch.ones((batch_size, num_queries, num_hiddens))
+d2l.check_shape(attention(X, X, X, valid_lens),
+                (batch_size, num_queries, num_hiddens))
