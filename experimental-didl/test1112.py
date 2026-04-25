2eb351a3eb4866cd99fa1af90c7cd0de7d2d8ff1
orientye <orientye@qq.com>
2026-04-25 10:28:30 +0800

    microai

--- experimental-didl/test1112.py
+++ experimental-didl/test1112.py
@@ -6,7 +6,8 @@ import torch
 from torch import nn
 from d2l import torch as d2l
 
-
+# num_hiddens: 模型的特征维度（隐藏层大小）。这里设为 100，意味着每个词或特征向量的长度是 100。
+# num_heads: 多头注意力的“头”数。这里设为 5，意味着 100 维的特征会被分成 5 组（每组 20 维）并行处理，最后再拼接起来
 num_hiddens, num_heads = 100, 5
 attention = d2l.MultiHeadAttention(num_hiddens, num_heads, 0.5)
 batch_size, num_queries, valid_lens = 2, 4, torch.tensor([3, 2])
