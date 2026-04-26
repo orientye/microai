774892c1d3b1e7c696a1775ca494ac6a5078943e
orientye <orientye@qq.com>
2026-04-26 16:14:58 +0800

    microai

--- experimental-didl/test1112.py
+++ experimental-didl/test1112.py
@@ -21,12 +21,12 @@ class PositionalEncoding(nn.Module):  # @save
 
     def __init__(self, num_hiddens, dropout, max_len=1000):
         super().__init__()
-        '''
+        """
         num_hiddens: 隐藏层维度（也是位置编码的维度）
         dropout: dropout比率，用于正则化
         max_len: 预计算的最大序列长度，默认为1000
         创建Dropout层用于防止过拟合
-        '''
+        """
         self.dropout = nn.Dropout(dropout)
         # Create a long enough P
         # 创建位置编码张量P，形状为(1, max_len, num_hiddens) 第一维为1是为批量维度预留
@@ -38,14 +38,21 @@ class PositionalEncoding(nn.Module):  # @save
         self.P[:, :, 1::2] = torch.cos(X) #奇数维度（1,3,5,...）使用余弦函数
 
     def forward(self, X):
+        """
+        这就是模型在训练时实际执行的动作。
+        X = X + self.P[:, :X.shape[1], :].to(X.device):
+        将输入的词向量X与对应的位置编码P直接相加。
+        :X.shape[1]是为了截取，使其长度和当前输入的序列长度一致。
+        .to(X.device)是为了确保位置矩阵和输入数据在同一个硬件（CPU或GPU）上。
+        """
         X = X + self.P[:, :X.shape[1], :].to(X.device)
-        return self.dropout(X)
+        return self.dropout(X) #返回加完位置信息并经过 Dropout处理后的结果。
 
 
 encoding_dim, num_steps = 32, 60
 pos_encoding = PositionalEncoding(encoding_dim, 0)
-X = pos_encoding(torch.zeros((1, num_steps, encoding_dim)))
-P = pos_encoding.P[:, :X.shape[1], :]
+X = pos_encoding(torch.zeros((1, num_steps, encoding_dim))) #创建一个全 0 的输入，通过编码器后，输出其实就只剩下位置编码本身了
+P = pos_encoding.P[:, :X.shape[1], :] #拿到计算好的位置矩阵
 d2l.plot(torch.arange(num_steps), P[0, :, 6:10].T, xlabel='Row (position)',
          figsize=(6, 2.5), legend=["Col %d" % d for d in torch.arange(6, 10)])
 plt.show()
