1e6a9ab1c597740bbb9b4ffb7abd13d5b69e367b
orientye <orientye@qq.com>
2026-05-05 14:59:16 +0800

    microai

--- experimental-didl/test1113.py
+++ experimental-didl/test1113.py
@@ -53,3 +53,54 @@ class AddNorm(nn.Module):  #@save
 add_norm = AddNorm(4, 0.5)
 shape = (2, 3, 4)
 d2l.check_shape(add_norm(torch.ones(shape), torch.ones(shape)), shape)
+
+
+class TransformerEncoderBlock(nn.Module):  #@save
+    """The Transformer encoder block."""
+    def __init__(self, num_hiddens, ffn_num_hiddens, num_heads, dropout,
+                 use_bias=False):
+        super().__init__()
+        self.attention = d2l.MultiHeadAttention(num_hiddens, num_heads,
+                                                dropout, use_bias)
+        self.addnorm1 = AddNorm(num_hiddens, dropout)
+        self.ffn = PositionWiseFFN(ffn_num_hiddens, num_hiddens)
+        self.addnorm2 = AddNorm(num_hiddens, dropout)
+
+    def forward(self, X, valid_lens):
+        Y = self.addnorm1(X, self.attention(X, X, X, valid_lens))
+        return self.addnorm2(Y, self.ffn(Y))
+
+X = torch.ones((2, 100, 24))
+valid_lens = torch.tensor([3, 2])
+encoder_blk = TransformerEncoderBlock(24, 48, 8, 0.5)
+encoder_blk.eval()
+d2l.check_shape(encoder_blk(X, valid_lens), X.shape)
+
+class TransformerEncoder(d2l.Encoder):  #@save
+    """The Transformer encoder."""
+    def __init__(self, vocab_size, num_hiddens, ffn_num_hiddens,
+                 num_heads, num_blks, dropout, use_bias=False):
+        super().__init__()
+        self.num_hiddens = num_hiddens
+        self.embedding = nn.Embedding(vocab_size, num_hiddens)
+        self.pos_encoding = d2l.PositionalEncoding(num_hiddens, dropout)
+        self.blks = nn.Sequential()
+        for i in range(num_blks):
+            self.blks.add_module("block"+str(i), TransformerEncoderBlock(
+                num_hiddens, ffn_num_hiddens, num_heads, dropout, use_bias))
+
+    def forward(self, X, valid_lens):
+        # Since positional encoding values are between -1 and 1, the embedding
+        # values are multiplied by the square root of the embedding dimension
+        # to rescale before they are summed up
+        X = self.pos_encoding(self.embedding(X) * math.sqrt(self.num_hiddens))
+        self.attention_weights = [None] * len(self.blks)
+        for i, blk in enumerate(self.blks):
+            X = blk(X, valid_lens)
+            self.attention_weights[
+                i] = blk.attention.attention.attention_weights
+        return X
+
+encoder = TransformerEncoder(200, 24, 48, 8, 2, 0.5)
+d2l.check_shape(encoder(torch.ones((2, 100), dtype=torch.long), valid_lens),
+                (2, 100, 24))
